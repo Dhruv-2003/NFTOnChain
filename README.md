505713f053c6efa6e0e95123a2fe97d90576b3b0
dhruv <dhruvagarwal2017@gmail.com>
2022-07-09 09:48:26 +0530

    New contract

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Contract deployed to: 0x37fE5cB2FFF0ebBdab69AdC5A4EcE3Cb8C311A0d
 
+Contract deployed to: 0x39813f50a9Ac1b1201EF7c32B80c78CD9fE8b46f
+
+npx hardhat run scripts/deploy.js --network mumbai
 npx hardhat verify --network mumbai <contract address>
 
 # Sample Hardhat Project
--- contracts/CricNFT.sol
+++ contracts/CricNFT.sol
@@ -11,8 +11,16 @@ contract CricNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    /// Player stuct containing all the player properties
+    struct players {
+        string name;
+        uint256 level;
+        uint256 runs;
+        uint256 wickets;
+    }
+
     /// mapping for the token ID => t levels
-    mapping(uint256 => uint256) public tokenIdLevels;
+    mapping(uint256 => players) public tokentoPlayer;
 
     /// intializing the NFT ERC721
     constructor() ERC721("Cric NFT", "Cric") {}
@@ -22,13 +30,22 @@ contract CricNFT is ERC721URIStorage {
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
-            '<rect width="100%" height="100%" fill="black" />',
-            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Cricket",
+            '<rect width="100%" height="100%" fill="blue" />',
+            '<text x="70%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Name: ",
+            getplayer(tokenId),
             "</text>",
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
             "Levels: ",
             getLevels(tokenId),
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Runs: ",
+            getruns(tokenId),
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Wickets: ",
+            getwickets(tokenId),
             "</text>",
             "</svg>"
         );
@@ -43,9 +60,27 @@ contract CricNFT is ERC721URIStorage {
     }
 
     /// to get the final level in the proper format
+    function getplayer(uint256 tokenId) public view returns (string memory) {
+        string memory _name = tokentoPlayer[tokenId].name;
+        return _name;
+    }
+
+    /// to get the final level in the proper format
     function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdLevels[tokenId];
+        uint256 levels = tokentoPlayer[tokenId].level;
         return levels.toString();
+    }
+
+    /// to get the final level in the proper format
+    function getruns(uint256 tokenId) public view returns (string memory) {
+        uint256 runs = tokentoPlayer[tokenId].runs;
+        return runs.toString();
+    }
+
+    /// to get the final level in the proper format
+    function getwickets(uint256 tokenId) public view returns (string memory) {
+        uint256 wickets = tokentoPlayer[tokenId].wickets;
+        return wickets.toString();
     }
 
     // to get the final tokenURI for a tokenId with metadata and svg together
@@ -55,7 +90,7 @@ contract CricNFT is ERC721URIStorage {
             '"name": "Cric NFT #',
             tokenId.toString(),
             '",',
-            '"description": "Cricket on Chain",',
+            '"description": "Cricket Players on Chain",',
             '"image": "',
             generateCharacter(tokenId),
             '"',
@@ -71,11 +106,14 @@ contract CricNFT is ERC721URIStorage {
     }
 
     /// to mint a on chain NFT using mint and setting a token URI for the svg
-    function mint() public {
+    function mint(string memory _name) public {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdLevels[newItemId] = 0;
+        tokentoPlayer[newItemId].name = _name;
+        tokentoPlayer[newItemId].level = 0;
+        tokentoPlayer[newItemId].runs = 0;
+        tokentoPlayer[newItemId].wickets = 0;
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
@@ -83,8 +121,10 @@ contract CricNFT is ERC721URIStorage {
     function train(uint256 tokenId) public {
         require(_exists(tokenId), "Please use an Exsisting token");
         require(ownerOf(tokenId) == msg.sender, "YOu are not the owner");
-        uint256 currentLevel = tokenIdLevels[tokenId];
-        tokenIdLevels[tokenId] = currentLevel + 1;
+        tokentoPlayer[tokenId].level += 1;
+        tokentoPlayer[tokenId].runs += 20;
+        tokentoPlayer[tokenId].wickets += 2;
+
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
